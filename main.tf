74783685b1bacea235524460fa05f22f7acd16c6
marabi01 <tripathirabi5@gmail.com>
2023-08-09 08:59:06 +0545

    initial commit

---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+    access_key = "AKIAU7MXCETT3OK5B4US"
+    secret_key = "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"
+    region = "us-east-2"
+  
+}
+
+resource "aws_vpc" "development-vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name: "development"
+    }
+  
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = "10.0.10.0/24" 
+    availability_zone = "us-east-2a"
+    
+    tags = {
+      Name: "dev-subnet"
+    }
+}
+
+
+variable "subnet_cidr_block" {
+    description = "subnet_cide_block"
+  
+}
+
+# using data
+
+data "aws_vpc" "existing_vpc" {
+    default = true
+  
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+    vpc_id = data.aws_vpc.existing_vpc.id
+    cidr_block = var.subnet_cide_block
+    availability_zone = "us-east-2b"
+    
+  
+}
+
+
+# checking the vpc id as a outbput
+
+output "existing_vpc-id" {
+    value = aws_subnet.dev-subnet-2.id
+  
+}
+
+
